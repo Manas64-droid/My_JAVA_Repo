2e147765cbbb19ffeae736b459f5e7cafbb16f90
Manas Mahadik <83745970+Manas64-droid@users.noreply.github.com>
2021-12-18 10:26:11 +0530

    Delete JAVA_PepCoding/AnujBhaiya directory

--- JAVA_PepCoding/AnujBhaiya/first.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>    
-// #include <conio.h>
-int main(){    
-printf("Hello C Language");    
-return 0;   
-} 
--- JAVA_PepCoding/AnujBhaiya/hello.java
+++
@@ -1,6 +0,0 @@
-public class heallo{
-	public static void main(String[]args){
-		System.out.println("Hello Manas");
-	}
-	
-}
